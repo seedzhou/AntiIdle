77315295ecf99438aea1d43ac76b82ca35681826
Yi Zhou <seedzhou@yahoo.com>
2018-07-22 16:28:20 -0500

    initial check in

---
+++ AntiIdle/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿using System.Windows.Forms;
+
+namespace AntiIdle
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.AntiIdleIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.statusToolStripMenuItem = new System.Windows.Forms.ToolStripLabel();
+            this.separator = new System.Windows.Forms.ToolStripSeparator();
+            this.actionAfterToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fiveMinsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tenMinsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.twentyMinsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thirtyMinsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oneHourToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.twoHoursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.threeHoursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fourHoursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eightHoursToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.neverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customizedToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.customizedTimeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox = new System.Windows.Forms.ToolStripTextBox();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.nowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // AntiIdleIcon
+            // 
+            this.AntiIdleIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("AntiIdleIcon.Icon")));
+            this.AntiIdleIcon.Text = "AntiIdleIcon";
+            this.AntiIdleIcon.Visible = true;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusToolStripMenuItem,
+            this.separator,
+            this.actionAfterToolStripMenuItem,
+            this.customizedTimeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(227, 116);
+            // 
+            // statusToolStripMenuItem
+            // 
+            this.statusToolStripMenuItem.Name = "statusToolStripMenuItem";
+            this.statusToolStripMenuItem.Size = new System.Drawing.Size(87, 15);
+            this.statusToolStripMenuItem.Text = "Status: Enabled";
+            // 
+            // separator
+            // 
+            this.separator.Name = "separator";
+            this.separator.Size = new System.Drawing.Size(223, 6);
+            // 
+            // actionAfterToolStripMenuItem
+            // 
+            this.actionAfterToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowToolStripMenuItem,
+            this.fiveMinsToolStripMenuItem,
+            this.tenMinsToolStripMenuItem,
+            this.twentyMinsToolStripMenuItem,
+            this.thirtyMinsToolStripMenuItem,
+            this.oneHourToolStripMenuItem,
+            this.twoHoursToolStripMenuItem,
+            this.threeHoursToolStripMenuItem,
+            this.fourHoursToolStripMenuItem,
+            this.eightHoursToolStripMenuItem,
+            this.neverToolStripMenuItem,
+            this.customizedToolStripMenuItem});
+            this.actionAfterToolStripMenuItem.Name = "actionAfterToolStripMenuItem";
+            this.actionAfterToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.actionAfterToolStripMenuItem.Text = "Pause After";
+            // 
+            // fiveMinsToolStripMenuItem
+            // 
+            this.fiveMinsToolStripMenuItem.Name = "fiveMinsToolStripMenuItem";
+            this.fiveMinsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fiveMinsToolStripMenuItem.Text = "5 minutes";
+            this.fiveMinsToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // tenMinsToolStripMenuItem
+            // 
+            this.tenMinsToolStripMenuItem.Name = "tenMinsToolStripMenuItem";
+            this.tenMinsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.tenMinsToolStripMenuItem.Text = "10 minutes";
+            this.tenMinsToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // twentyMinsToolStripMenuItem
+            // 
+            this.twentyMinsToolStripMenuItem.Name = "twentyMinsToolStripMenuItem";
+            this.twentyMinsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.twentyMinsToolStripMenuItem.Text = "20 minutes";
+            this.twentyMinsToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // thirtyMinsToolStripMenuItem
+            // 
+            this.thirtyMinsToolStripMenuItem.Name = "thirtyMinsToolStripMenuItem";
+            this.thirtyMinsToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.thirtyMinsToolStripMenuItem.Text = "30 minutes";
+            this.thirtyMinsToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // oneHourToolStripMenuItem
+            // 
+            this.oneHourToolStripMenuItem.Name = "oneHourToolStripMenuItem";
+            this.oneHourToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.oneHourToolStripMenuItem.Text = "1 hour";
+            this.oneHourToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // twoHoursToolStripMenuItem
+            // 
+            this.twoHoursToolStripMenuItem.Name = "twoHoursToolStripMenuItem";
+            this.twoHoursToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.twoHoursToolStripMenuItem.Text = "2 hours";
+            this.twoHoursToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // threeHoursToolStripMenuItem
+            // 
+            this.threeHoursToolStripMenuItem.Name = "threeHoursToolStripMenuItem";
+            this.threeHoursToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.threeHoursToolStripMenuItem.Text = "3 hours";
+            this.threeHoursToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // fourHoursToolStripMenuItem
+            // 
+            this.fourHoursToolStripMenuItem.Name = "fourHoursToolStripMenuItem";
+            this.fourHoursToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.fourHoursToolStripMenuItem.Text = "4 hours";
+            this.fourHoursToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // eightHoursToolStripMenuItem
+            // 
+            this.eightHoursToolStripMenuItem.Name = "eightHoursToolStripMenuItem";
+            this.eightHoursToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.eightHoursToolStripMenuItem.Text = "8 hours";
+            this.eightHoursToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // neverToolStripMenuItem
+            // 
+            this.neverToolStripMenuItem.Name = "neverToolStripMenuItem";
+            this.neverToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.neverToolStripMenuItem.Text = "Never";
+            this.neverToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // customizedToolStripMenuItem
+            // 
+            this.customizedToolStripMenuItem.Name = "customizedToolStripMenuItem";
+            this.customizedToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.customizedToolStripMenuItem.Text = "Customized";
+            this.customizedToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // customizedTimeToolStripMenuItem
+            // 
+            this.customizedTimeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox});
+            this.customizedTimeToolStripMenuItem.Name = "customizedTimeToolStripMenuItem";
+            this.customizedTimeToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.customizedTimeToolStripMenuItem.Text = "Customized Time in Minutes";
+            // 
+            // toolStripTextBox
+            // 
+            this.toolStripTextBox.Name = "toolStripTextBox";
+            this.toolStripTextBox.Size = new System.Drawing.Size(100, 23);
+            this.toolStripTextBox.Text = "60";
+            this.toolStripTextBox.TextChanged += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.TimerOnTick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // nowToolStripMenuItem
+            // 
+            this.nowToolStripMenuItem.Name = "nowToolStripMenuItem";
+            this.nowToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.nowToolStripMenuItem.Text = "Now";
+            this.nowToolStripMenuItem.Click += new System.EventHandler(this.toolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Name = "Form1";
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private NotifyIcon AntiIdleIcon;
+        private ContextMenuStrip contextMenuStrip;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripLabel statusToolStripMenuItem;
+        private ToolStripMenuItem actionAfterToolStripMenuItem;
+        private ToolStripMenuItem fiveMinsToolStripMenuItem;
+        private ToolStripMenuItem tenMinsToolStripMenuItem;
+        private ToolStripMenuItem twentyMinsToolStripMenuItem;
+        private ToolStripMenuItem thirtyMinsToolStripMenuItem;
+        private ToolStripMenuItem oneHourToolStripMenuItem;
+        private ToolStripMenuItem twoHoursToolStripMenuItem;
+        private ToolStripMenuItem threeHoursToolStripMenuItem;
+        private ToolStripMenuItem fourHoursToolStripMenuItem;
+        private ToolStripMenuItem eightHoursToolStripMenuItem;
+        private ToolStripMenuItem neverToolStripMenuItem;
+        private Timer timer;
+        private ToolStripMenuItem customizedToolStripMenuItem;
+        private ToolStripMenuItem customizedTimeToolStripMenuItem;
+        private ToolStripTextBox toolStripTextBox;
+        private Timer timer1;
+        private ToolStripSeparator separator;
+        private ToolStripMenuItem nowToolStripMenuItem;
+    }
+}
+
---
+++ AntiIdle/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AntiIdle
+{
+    public partial class Form1 : Form
+    {
+        private const int minute = 60000;
+
+        public Form1()
+        {
+            InitializeComponent();
+            AntiIdleIcon.ContextMenuStrip = contextMenuStrip;
+            timer.Interval = 299000;
+            timer.Enabled = true;
+            timer1.Enabled = false;
+            toolStripMenuItem_Click(fourHoursToolStripMenuItem, null);
+        }
+
+        private void TimerOnTick(object sender, EventArgs eventArgs)
+        {
+            SendInputClass.MoveMouse();
+        }
+
+        private bool allowVisible;     // ContextMenu's Show command used
+        private bool allowClose;       // ContextMenu's Exit command used
+
+        protected override void SetVisibleCore(bool value)
+        {
+            if (!allowVisible)
+            {
+                value = false;
+                if (!this.IsHandleCreated) CreateHandle();
+            }
+            base.SetVisibleCore(value);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (!allowClose)
+            {
+                this.Hide();
+                e.Cancel = true;
+            }
+            base.OnFormClosing(e);
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            allowVisible = true;
+            Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AntiIdleIcon.Icon = null;
+            AntiIdleIcon.Dispose();
+            allowClose = true;
+            Application.Exit();
+        }
+
+        private void toolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (sender is ToolStripTextBox)
+            {
+                sender = customizedToolStripMenuItem;
+            }
+
+            ResetAllToolStripMenuItems((ToolStripMenuItem)sender);
+            int delayTime = GetDelayTime((ToolStripMenuItem)sender);
+
+            timer1.Interval = delayTime;
+            timer1.Enabled = true;
+
+            statusToolStripMenuItem.Text = "Status: Enabled";
+            actionAfterToolStripMenuItem.Text = "Pause After";
+            nowToolStripMenuItem.Visible = true;
+            timer.Enabled = true;
+
+            if (sender == neverToolStripMenuItem)
+            {
+                timer1.Enabled = false;
+            }
+        }
+
+        private int GetDelayTime(ToolStripMenuItem sender)
+        {
+            var delayTime = 180 * minute;
+
+            if (sender == nowToolStripMenuItem)
+            {
+                delayTime = 1000;
+            }
+            else if (sender == fiveMinsToolStripMenuItem)
+            {
+                delayTime = 5 * minute;
+            }
+            else if (sender == tenMinsToolStripMenuItem)
+            {
+                delayTime = 10 * minute;
+            }
+            else if (sender == twentyMinsToolStripMenuItem)
+            {
+                delayTime = 20 * minute;
+            }
+            else if (sender == thirtyMinsToolStripMenuItem)
+            {
+                delayTime = 30 * minute;
+            }
+            else if (sender == oneHourToolStripMenuItem)
+            {
+                delayTime = 60 * minute;
+            }
+            else if (sender == twoHoursToolStripMenuItem)
+            {
+                delayTime = 120 * minute;
+            }
+            else if (sender == threeHoursToolStripMenuItem)
+            {
+                delayTime = 180 * minute;
+            }
+            else if (sender == fourHoursToolStripMenuItem)
+            {
+                delayTime = 240 * minute;
+            }
+            else if (sender == eightHoursToolStripMenuItem)
+            {
+                delayTime = 480 * minute;
+            }
+            else if (sender == customizedToolStripMenuItem)
+            {
+                delayTime = Convert.ToInt32(toolStripTextBox.Text) * minute;
+            }
+
+            return delayTime;
+        }
+
+        private void ResetAllToolStripMenuItems(ToolStripMenuItem toolStripMenuItem)
+        {
+            SetToolStripMenuItem(nowToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(fiveMinsToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(tenMinsToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(twentyMinsToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(thirtyMinsToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(oneHourToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(twoHoursToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(threeHoursToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(fourHoursToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(eightHoursToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(customizedToolStripMenuItem, toolStripMenuItem);
+            SetToolStripMenuItem(neverToolStripMenuItem, toolStripMenuItem);
+        }
+
+        private void SetToolStripMenuItem(ToolStripMenuItem toolStripMenuItem, ToolStripMenuItem clickedToolStripMenuItem)
+        {
+            toolStripMenuItem.Checked = toolStripMenuItem == clickedToolStripMenuItem;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+
+            statusToolStripMenuItem.Text = "Status: Disabled";
+            actionAfterToolStripMenuItem.Text = "Resume and Pause After";
+            nowToolStripMenuItem.Visible = false;
+            timer.Enabled = false;
+        }
+    }
+
+    public static class SendInputClass
+    {
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern uint SendInput(uint nInputs, ref INPUT pInputs, int cbSize);
+
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(out Point lpPoint);
+
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        struct INPUT
+        {
+            public SendInputEventType type;
+            public MouseKeybdhardwareInputUnion mkhi;
+        }
+        [StructLayout(LayoutKind.Explicit)]
+        struct MouseKeybdhardwareInputUnion
+        {
+            [FieldOffset(0)]
+            public MouseInputData mi;
+
+            [FieldOffset(0)]
+            public KEYBDINPUT ki;
+
+            [FieldOffset(0)]
+            public HARDWAREINPUT hi;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        struct KEYBDINPUT
+        {
+            public ushort wVk;
+            public ushort wScan;
+            public uint dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+        [StructLayout(LayoutKind.Sequential)]
+        struct HARDWAREINPUT
+        {
+            public int uMsg;
+            public short wParamL;
+            public short wParamH;
+        }
+        struct MouseInputData
+        {
+            public int dx;
+            public int dy;
+            public uint mouseData;
+            public MouseEventFlags dwFlags;
+            public uint time;
+            public IntPtr dwExtraInfo;
+        }
+        [Flags]
+        enum MouseEventFlags : uint
+        {
+            MOUSEEVENTF_MOVE = 0x0001,
+            MOUSEEVENTF_LEFTDOWN = 0x0002,
+            MOUSEEVENTF_LEFTUP = 0x0004,
+            MOUSEEVENTF_RIGHTDOWN = 0x0008,
+            MOUSEEVENTF_RIGHTUP = 0x0010,
+            MOUSEEVENTF_MIDDLEDOWN = 0x0020,
+            MOUSEEVENTF_MIDDLEUP = 0x0040,
+            MOUSEEVENTF_XDOWN = 0x0080,
+            MOUSEEVENTF_XUP = 0x0100,
+            MOUSEEVENTF_WHEEL = 0x0800,
+            MOUSEEVENTF_VIRTUALDESK = 0x4000,
+            MOUSEEVENTF_ABSOLUTE = 0x8000
+        }
+        enum SendInputEventType : int
+        {
+            InputMouse,
+            InputKeyboard,
+            InputHardware
+        }
+
+        public static void ClickLeftMouseButton(int x, int y)
+        {
+            INPUT mouseInput = new INPUT();
+            mouseInput.type = SendInputEventType.InputMouse;
+            mouseInput.mkhi.mi.dx = x;
+            mouseInput.mkhi.mi.dy = y;
+            mouseInput.mkhi.mi.mouseData = 0;
+
+            //getting current cursor location
+            Point p;
+            if (GetCursorPos(out p))
+                SetCursorPos(x, y);
+
+
+            mouseInput.mkhi.mi.dwFlags = MouseEventFlags.MOUSEEVENTF_LEFTDOWN;
+            SendInput(1, ref mouseInput, Marshal.SizeOf(new INPUT()));
+
+            mouseInput.mkhi.mi.dwFlags = MouseEventFlags.MOUSEEVENTF_LEFTUP;
+            SendInput(1, ref mouseInput, Marshal.SizeOf(new INPUT()));
+
+            //returning cursor to previous position
+            SetCursorPos(p.X, p.Y);
+        }
+
+        public static void MoveMouse()
+        {
+            //getting current cursor location
+            Point p;
+            GetCursorPos(out p);
+
+            INPUT input = new INPUT();
+            input.type = SendInputEventType.InputMouse;
+            input.mkhi.mi.dx = p.X;
+            input.mkhi.mi.dy = p.Y;
+            input.mkhi.mi.mouseData = 0;
+
+            input.mkhi.mi.dwFlags = MouseEventFlags.MOUSEEVENTF_MOVE;
+            //SendInput(1, ref input, Marshal.SizeOf(new INPUT()));
+
+            input.type = SendInputEventType.InputKeyboard;
+            input.mkhi.ki.wVk = 0x90;
+            input.mkhi.ki.dwFlags = 0x0002;
+            //SendInput(1, ref input, Marshal.SizeOf(new INPUT()));
+            SendInput(1, ref input, Marshal.SizeOf(new INPUT()));
+
+            //returning cursor to previous position
+            SetCursorPos(p.X, p.Y);
+        }
+    }
+}
